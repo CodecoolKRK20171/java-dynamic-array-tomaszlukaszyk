416ca02d4dc9d7d9c0b54cb50a5309825f91f9b0
Tomasz Łukaszyk <el.moue@gmail.com>
2018-12-04 18:11:29 +0100

    Implement DynamicIntArray

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,6 +1,83 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
 
+    private final int INITIAL_SIZE = 10;
+
+    private int[] array;
+    private int size;
+
+    public DynamicIntArray() {
+        this.array = new int[INITIAL_SIZE];
+    }
+
+    public DynamicIntArray(int size) {
+        this.array = new int[size];
+        this.size = size;
+    }
+
+    public void add(int element) {
+        increaseSize();
+        this.array[this.size -1] = element;
+    }
+
+    private void increaseSize() {
+        this.size++;
+        if (this.array.length < this.size) {
+            this.array = Arrays.copyOf(array, this.size + INITIAL_SIZE);
+        }
+    }
+
+    public void remove(int element) {
+        int searchedIndex = findIndexOfElement(element);
+        if (searchedIndex == -1) throw new ArrayIndexOutOfBoundsException();
+
+        for (int i = searchedIndex; i<this.size-1; i++) {
+            this.array[i] = this.array[i+1];
+        }
+
+        decreaseSize();
+    }
+
+    private int findIndexOfElement(int element) {
+        for (int i = 0; i<this.size; i++) {
+            if (this.array[i] == element) return i;
+        }
+        return -1;
+    }
+
+    private void decreaseSize() {
+        this.size--;
+        if (this.array.length > this.size + INITIAL_SIZE) {
+            this.array = Arrays.copyOf(array, array.length - INITIAL_SIZE);
+        }
+    }
+
+    public void insert(int index, int element) {
+        if (index > this.size) {
+            add(element);
+            return;
+        }
+
+        increaseSize();
+
+        for (int i = this.size; i>index; i--) {
+            array[i] = array[i-1];
+        }
+
+        array[index] = element;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i<this.size; i++) {
+            sb.append(" ");
+            sb.append(array[i]);
+        }
+        return sb.toString();
+    }
 }
